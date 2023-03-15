f4043ea2cb1ddea92e742320115256ed248921a0
jorge-bonilla <52008776+jorge-bonilla@users.noreply.github.com>
2023-03-15 11:10:18 -0600

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-    1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": { "Jorge": "it's good"} },
-    2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": { "Michael": "Alright"} },
-    3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": { "Hugo": "wouldn't read it again"} },
-    4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": { "Eli": "fully recommended reading"} },
-    5: {"author": "Unknown","title": "The Book Of Job", "reviews": { "Brian": "It's in my top 5 books"} },
-    6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": { "Eunice": "I do not recommend it"} },
-    7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": { "Daniela": "better read the bible"} },
-    8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": { "Alejandro": "very boring, very slow pace"} },
-    9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": { "Nabucodonosor": "Best read I've had in years"} },
-    10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": { "Luis": "I cried"} }
+    1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": [{ "username": "Jorge", "review": "it's good"}] },
+    2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": [{ "username": "Michael", "review": "Alright"}] },
+    3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": [{ "username": "Samuel", "review": "wouldn't read it again"}] },
+    4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": [{ "username": "Eli", "review": "fully recommended reading"}] },
+    5: {"author": "Unknown","title": "The Book Of Job", "reviews": [{ "username": "Hugo", "review": "It's in my top 5 books"}] },
+    6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": [{ "username": "Eunice", "review": "I do not recommend it"}] },
+    7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": [{ "username": "Daniel", "review": "better read the bible"}] },
+    8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": [{ "username": "Alejandro", "review": "very boring, very slow pace"}] },
+    9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": [{ "username": "Brian", "review": "Best read I've had in years"}] },
+    10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": [{ "username": "Nabucodonor", "review": "I cried"}] }
 }
 
 module.exports=books;
