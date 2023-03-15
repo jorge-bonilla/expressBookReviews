b0550e5a8a69a6531a7d0080fc8c30c29506d6a1
jorge-bonilla <52008776+jorge-bonilla@users.noreply.github.com>
2023-03-15 11:54:06 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,29 +22,66 @@ public_users.post("/register", (req,res) => {
 
 });
 
-// Get the book list available in the shop
+// task 1 - Get the book list available in the shop
 public_users.get('/',function (req, res) {
   res.send(JSON.stringify({books},null,4));
 });
 
-// Get book details based on ISBN
+// TASK 10 - Get the book list available in the shop using promises
+public_users.get('/books',function (req, res) {
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+  });
+
+// task 2 - Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   res.send(books[isbn]);
 });
+
+// TASK 11 - Get book details based on ISBN using promises
+public_users.get('/isbn_t11/:isbn',function (req, res) {
+    const get_book = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        res.send(books[isbn]);
+      });
+      get_book.then(() => console.log("Promise for Task 11 resolved"));
+  });
   
-// Get book details based on author
+// Task 3 - Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     let filtered_books=Object.values(books).filter((book) => book.author === author);
     res.send(filtered_books);
 });
+
+// TASK 12 - Get book details based on author  using promises
+public_users.get('/author_t12/:author',function (req, res) {
+    const get_book = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        let filtered_books=Object.values(books).filter((book) => book.author === author);
+        res.send(filtered_books);
+      });
+      get_book.then(() => console.log("Promise for Task 12 resolved"));
+});
   
-// Get all books based on title
+// task 4 - Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     let filtered_books=Object.values(books).filter((book) => book.title === title);
     res.send(filtered_books);
+});
+
+// TASK 13 - Get all books based on title  using promises
+public_users.get('/title_t13/:title',function (req, res) {
+    const get_book = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        let filtered_books=Object.values(books).filter((book) => book.title === title);
+        res.send(filtered_books);
+      });
+      get_book.then(() => console.log("Promise for Task 12 resolved"));
 });
 
 //  Get book review
